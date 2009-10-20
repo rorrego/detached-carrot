76fc70a89a7308fd748f41d537044adc707e642d
Rodrigo Orrego <rorrego@gmail.com>
2009-10-20 11:34:15 -0300

    se agrega /script para llamarlo como daemon

---
+++ script/listen.rb
@@ -0,0 +1,41 @@
+puts '=> Loading Rails...'
+
+require File.dirname(__FILE__) + '/../../../../config/environment'
+
+invoker = DetachedCarrot::Base
+poller = invoker.new()
+puts '** Rails loaded.'
+puts "** Starting #{ invoker }..."
+puts '** Use CTRL-C to stop.'
+
+ActiveRecord::Base.logger = DetachedCarrot::Base.logger
+ActionController::Base.logger = DetachedCarrot::Base.logger
+
+trap(:INT) { poller.stop; exit }
+
+begin
+  poller.listen
+ensure
+  puts '** No Pollers found.'
+  puts "** Exiting at #{Time.now}"
+end
+
+def tail(log_file)
+  cursor = File.size(log_file)
+  last_checked = Time.now
+  tail_thread = Thread.new do
+    File.open(log_file, 'r') do |f|
+      loop do
+        f.seek cursor
+        if f.mtime > last_checked
+          last_checked = f.mtime
+          contents = f.read
+          cursor += contents.length
+          print contents
+        end
+        sleep 1
+      end
+    end
+  end
+  tail_thread
+end
